5d94115dc2a6db45f7b4fc7efbf760e22190275f
Felipe <felipetermometria@hotmail.com>
2023-11-22 17:45:10 -0300

    Box(modifier = Modifier         .fillMaxSize()         .background(Color.Black)     )

--- app/src/main/java/com/example/app_dicegames/MainActivity.kt
+++ app/src/main/java/com/example/app_dicegames/MainActivity.kt
@@ -3,12 +3,15 @@ package com.example.app_dicegames
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.app_dicegames.ui.theme.App_DiceGamesTheme
 
@@ -31,7 +34,11 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun App() {
-    Text("Ola")
+    Box(modifier = Modifier
+        .fillMaxSize()
+        .background(Color.Black)
+    )
+
 }
 
 @Preview(showBackground = true)
